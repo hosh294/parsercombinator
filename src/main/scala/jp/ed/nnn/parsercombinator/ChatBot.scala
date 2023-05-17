7cf714e4d78fef3b1a85305b63a8036816c72c82
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-17 14:51:36 +0900

    birthdayCommandの実装

--- src/main/scala/jp/ed/nnn/parsercombinator/ChatBot.scala
+++ src/main/scala/jp/ed/nnn/parsercombinator/ChatBot.scala
@@ -1,21 +1,48 @@
 package jp.ed.nnn.parsercombinator
 
+import java.time.{LocalDateTime, ZoneId}
 import scala.util.Random
 import scala.util.matching.Regex
 
 case class ChatBot(commands: List[Command])
 
 sealed trait Command {
-  def exec(input :String): Boolean
+  def exec(input: String): Boolean
 }
 
-case class ReplyCommand(regex: Regex, replays: List[String]) extends Command {
-  override def exec(input :String): Boolean = {
+case class ReplyCommand(regex: Regex, replies: List[String]) extends Command {
+  override def exec(input: String): Boolean = {
     regex.findFirstIn(input) match {
       case Some(_) =>
-        println(Random.shuffle(replays).head)
+        println(Random.shuffle(replies).head)
         true
       case None => false
+    }
+  }
+}
+
+case class TimeCommand(regex: Regex, start: Int, end: Int, zone: String, replies: List[String]) extends Command {
+  override def exec(input: String): Boolean = {
+    val now = LocalDateTime.now().atZone(ZoneId.of(zone))
+    val isInTime = start <= now.getHour && now.getHour <= end
+    regex.findFirstIn(input) match {
+      case Some(_) if isInTime =>
+        println(Random.shuffle(replies).head)
+        true
+      case _ => false
+    }
+  }
+}
+
+case class BirthdayCommand(regex: Regex, month: Int, day: Int, zone: String, replies: List[String]) extends Command {
+  override def exec(input: String): Boolean = {
+    val now = LocalDateTime.now().atZone(ZoneId.of(zone))
+    val isBirthday = now.getMonth.getValue == month && now.getDayOfMonth == day
+    regex.findFirstIn(input) match {
+      case Some(_) if isBirthday =>
+        println(Random.shuffle(replies).head)
+        true
+      case _ => false
     }
   }
 }
