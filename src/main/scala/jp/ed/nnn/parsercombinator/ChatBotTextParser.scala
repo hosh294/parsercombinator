06f9972c96b849b3b0ec83113576d839af0d6372
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-17 14:52:30 +0900

    birthdayCommandの実装

--- src/main/scala/jp/ed/nnn/parsercombinator/ChatBotTextParser.scala
+++ src/main/scala/jp/ed/nnn/parsercombinator/ChatBotTextParser.scala
@@ -4,18 +4,42 @@ import scala.util.parsing.combinator._
 
 object ChatBotTextParser extends JavaTokenParsers {
 
-  def chatBot: Parser[ChatBot] =  "(" ~ "chatbot" ~ commandList ~ ")" ^^ {t => ChatBot(t._1._2)}
+  def chatBot: Parser[ChatBot] = "(" ~ "chatbot" ~ commandList ~ ")" ^^ { t => ChatBot(t._1._2) }
 
   def commandList: Parser[List[Command]] = rep(command)
 
-  def command: Parser[Command] = replyCommand
+  def command: Parser[Command] = replyCommand | timeCommand | birthdayCommand
 
   def replyCommand: Parser[ReplyCommand] =
-    "(" ~ "reply" ~ string ~ replyList ~ ")" ^^ {t => ReplyCommand(t._1._1._2.r, t._1._2) }
+    "(" ~ "reply" ~ string ~ replyList ~ ")" ^^ { t => ReplyCommand(t._1._1._2.r, t._1._2) }
 
-  def replyList: Parser[List[String]] = "(" ~ rep(string) ~ ")" ^^ { t => t._1._2}
+  def timeCommand: Parser[TimeCommand] =
+    "(" ~ "time" ~ string ~ digits ~ digits ~ string ~ replyList ~ ")" ^^ { t =>
+      TimeCommand(
+        t._1._1._1._1._1._2.r,
+        t._1._1._1._1._2.toInt,
+        t._1._1._1._2.toInt,
+        t._1._1._2,
+        t._1._2)
+    }
 
-  def string: Parser[String] =  stringLiteral ^^ { s => s.substring(1, s.length - 1) }
+  def birthdayCommand: Parser[BirthdayCommand] =
+    "(" ~ "birthday" ~ string ~ digits ~ digits ~ string ~ replyList ~ ")" ^^ {
+      t =>
+        BirthdayCommand(
+          t._1._1._1._1._1._2.r,
+          t._1._1._1._1._2.toInt,
+          t._1._1._1._2.toInt,
+          t._1._1._2,
+          t._1._2)
+    }
+
+
+  def digits: Parser[String] = "[0-9]+".r
+
+  def replyList: Parser[List[String]] = "(" ~ rep(string) ~ ")" ^^ { t => t._1._2 }
+
+  def string: Parser[String] = stringLiteral ^^ { s => s.substring(1, s.length - 1) }
 
   def apply(input: String): ParseResult[ChatBot] = parseAll(chatBot, input)
 
